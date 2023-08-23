bc7ce7da7a1fb3d42a1d279b43b3f7ed2ff104c8
JacksonRT <jacksontower8@gmail.com>
2023-08-23 16:21:58 -0400

    Commit 1

---
+++ Code/main.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+
+pygame.display.set_caption('Connectz')
+
+screen = pygame.display.set_mode([800, 800])
+fps = 60
+
+def draw_window():
+    screen.fill((78, 208, 225))
+    pygame.display.update()
+
+
+clock = pygame.time.Clock()
+running = True
+while running:
+    clock.tick(fps)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    draw_window()
+
+
+pygame.quit()
